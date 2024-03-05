3334aed1bc67516aa49fb2fff4a0412ee3dca36e
Chang Liu <liuc223@gmail.com>
2024-03-05 21:42:47 +0800

    Set redirection for old blogs

--- assets/js/redirect.js
+++
@@ -1,7 +0,0 @@
-const htmlName = window.location.pathname.split("/").pop();
-const htmlNameSplit = htmlName.split(".");
-htmlNameSplit.pop();
-const htmlNameWithoutExt = htmlNameSplit.join(".");
-
-const redirectUrl = "/docusaurus/blog/" + htmlNameWithoutExt;
-window.location.replace(redirectUrl);
